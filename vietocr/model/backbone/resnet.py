1eea70a92310fa8005bf5f4073d3cfd23130bd34
KHOA TRAN <48320778+khoatranrb@users.noreply.github.com>
2022-04-09 16:17:01 +0700

    Update resnet.py

--- vietocr/model/backbone/resnet.py
+++ vietocr/model/backbone/resnet.py
@@ -1,5 +1,32 @@
+import numpy as np
+import math
+from typing import Tuple
 import torch
-from torch import nn
+from torch import nn, Tensor
+import torch.nn.functional as F
+from torch.nn import TransformerEncoder, TransformerEncoderLayer
+from torch.utils.data import dataset
+import copy
+import random
+
+class Step(nn.Module):
+    def __init__(self, module, alpha=0.99, eps=1e-8):
+        super(Step, self).__init__()
+        self.x = None
+        self.mod = module
+        self.xxx = 0.5
+
+    def forward(self, inp=None):
+        try:
+            self.xxx = self.xxx*1.1-0.1*((self.x.grad*(inp-self.x))>0).sum().item()/np.prod(list(inp.size()))
+        except: pass
+        self.x = nn.Parameter(inp)
+        out1 = self.mod(self.x)
+        out2 = self.mod(inp)
+        scale = math.sqrt(float(self.xxx))
+#         if random.random() < 0.001:
+#             print(scale)
+        return scale*out2+out1*(1-scale)
 
 class BasicBlock(nn.Module):
     expansion = 1
@@ -16,8 +43,8 @@ class BasicBlock(nn.Module):
 
     def _conv3x3(self, in_planes, out_planes, stride=1):
         "3x3 convolution with padding"
-        return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
-                         padding=1, bias=False)
+        return Step(nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                         padding=1, bias=False))
 
     def forward(self, x):
         residual = x
@@ -44,46 +71,46 @@ class ResNet(nn.Module):
         self.output_channel_block = [int(output_channel / 4), int(output_channel / 2), output_channel, output_channel]
 
         self.inplanes = int(output_channel / 8)
-        self.conv0_1 = nn.Conv2d(input_channel, int(output_channel / 16),
-                                 kernel_size=3, stride=1, padding=1, bias=False)
+        self.conv0_1 = Step(nn.Conv2d(input_channel, int(output_channel / 16),
+                                 kernel_size=3, stride=1, padding=1, bias=False))
         self.bn0_1 = nn.BatchNorm2d(int(output_channel / 16))
-        self.conv0_2 = nn.Conv2d(int(output_channel / 16), self.inplanes,
-                                 kernel_size=3, stride=1, padding=1, bias=False)
+        self.conv0_2 = Step(nn.Conv2d(int(output_channel / 16), self.inplanes,
+                                 kernel_size=3, stride=1, padding=1, bias=False))
         self.bn0_2 = nn.BatchNorm2d(self.inplanes)
         self.relu = nn.ReLU(inplace=True)
 
         self.maxpool1 = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
         self.layer1 = self._make_layer(block, self.output_channel_block[0], layers[0])
-        self.conv1 = nn.Conv2d(self.output_channel_block[0], self.output_channel_block[
-                               0], kernel_size=3, stride=1, padding=1, bias=False)
+        self.conv1 = Step(nn.Conv2d(self.output_channel_block[0], self.output_channel_block[
+                               0], kernel_size=3, stride=1, padding=1, bias=False))
         self.bn1 = nn.BatchNorm2d(self.output_channel_block[0])
 
         self.maxpool2 = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
         self.layer2 = self._make_layer(block, self.output_channel_block[1], layers[1], stride=1)
-        self.conv2 = nn.Conv2d(self.output_channel_block[1], self.output_channel_block[
-                               1], kernel_size=3, stride=1, padding=1, bias=False)
+        self.conv2 = Step(nn.Conv2d(self.output_channel_block[1], self.output_channel_block[
+                               1], kernel_size=3, stride=1, padding=1, bias=False))
         self.bn2 = nn.BatchNorm2d(self.output_channel_block[1])
 
         self.maxpool3 = nn.MaxPool2d(kernel_size=2, stride=(2, 1), padding=(0, 1))
         self.layer3 = self._make_layer(block, self.output_channel_block[2], layers[2], stride=1)
-        self.conv3 = nn.Conv2d(self.output_channel_block[2], self.output_channel_block[
-                               2], kernel_size=3, stride=1, padding=1, bias=False)
+        self.conv3 = Step(nn.Conv2d(self.output_channel_block[2], self.output_channel_block[
+                               2], kernel_size=3, stride=1, padding=1, bias=False))
         self.bn3 = nn.BatchNorm2d(self.output_channel_block[2])
 
         self.layer4 = self._make_layer(block, self.output_channel_block[3], layers[3], stride=1)
-        self.conv4_1 = nn.Conv2d(self.output_channel_block[3], self.output_channel_block[
-                                 3], kernel_size=2, stride=(2, 1), padding=(0, 1), bias=False)
+        self.conv4_1 = Step(nn.Conv2d(self.output_channel_block[3], self.output_channel_block[
+                                 3], kernel_size=2, stride=(2, 1), padding=(0, 1), bias=False))
         self.bn4_1 = nn.BatchNorm2d(self.output_channel_block[3])
-        self.conv4_2 = nn.Conv2d(self.output_channel_block[3], self.output_channel_block[
-                                 3], kernel_size=2, stride=1, padding=0, bias=False)
+        self.conv4_2 = Step(nn.Conv2d(self.output_channel_block[3], self.output_channel_block[
+                                 3], kernel_size=2, stride=1, padding=0, bias=False))
         self.bn4_2 = nn.BatchNorm2d(self.output_channel_block[3])
 
     def _make_layer(self, block, planes, blocks, stride=1):
         downsample = None
         if stride != 1 or self.inplanes != planes * block.expansion:
             downsample = nn.Sequential(
-                nn.Conv2d(self.inplanes, planes * block.expansion,
-                          kernel_size=1, stride=stride, bias=False),
+                Step(nn.Conv2d(self.inplanes, planes * block.expansion,
+                          kernel_size=1, stride=stride, bias=False)),
                 nn.BatchNorm2d(planes * block.expansion),
             )
 
