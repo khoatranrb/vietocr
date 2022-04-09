9e1442e88e41782b7e913cb766a6ab4480630797
KHOA TRAN <48320778+khoatranrb@users.noreply.github.com>
2022-04-09 21:25:20 +0700

    Update trainer.py

--- vietocr/model/trainer.py
+++ vietocr/model/trainer.py
@@ -61,7 +61,8 @@ class Trainer():
 
         self.iter = 0
         
-        self.optimizer = AdamW(self.model.parameters(), betas=(0.9, 0.98), eps=1e-09)
+#         self.optimizer = AdamW(self.model.parameters(), betas=(0.9, 0.98), eps=1e-09)
+        self.optimizer = Adam(self.model.parameters(), lr=5e-4)
         self.scheduler = OneCycleLR(self.optimizer, total_steps=self.num_iters, **config['optimizer'])
 #        self.optimizer = ScheduledOptim(
 #            Adam(self.model.parameters(), betas=(0.9, 0.98), eps=1e-09),
